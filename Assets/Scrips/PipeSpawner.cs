5d98ab44e593def9d1a18190b131101c2971c6ed
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2023-11-22 14:07:09 +0100

    add spawner

--- Assets/Scrips/PipeSpawner.cs
+++ Assets/Scrips/PipeSpawner.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PipeSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _maxTime = 1.5f;
+    [SerializeField] private float _heightRange = .45f;
+    [SerializeField] private GameObject _pipe;
+
+    private float _timer = 0;
+
+    //private void Start() => Spawn();
+
+    private void Update()
     {
-        
+        if (_timer > _maxTime)
+        {
+            Spawn();
+            _timer = 0;
+        }
+        _timer += Time.deltaTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Spawn()
     {
-        
+        Vector3 spawnPos = transform.position + new Vector3(0, 
+            Random.Range(-_heightRange, +_heightRange));
+
+        GameObject pipe = Instantiate(_pipe, spawnPos, Quaternion.identity);
+
+        Destroy(pipe, 10f);
     }
 }
