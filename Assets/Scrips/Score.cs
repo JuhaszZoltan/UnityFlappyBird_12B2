787472b3c8dbd09772c6d1778a73ddae38859935
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2023-12-04 09:35:03 +0100

    fin

---
+++ Assets/Scrips/Score.cs
@@ -0,0 +1,42 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public static Score instance;
+
+    [SerializeField] private TextMeshProUGUI _currentScoreText;
+    [SerializeField] private TextMeshProUGUI _bestScoreText;
+
+    private int _score;
+
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    public void Start()
+    {
+        _currentScoreText.text = _score.ToString();
+        _bestScoreText.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+        //UpdateHighScore();
+    }
+
+    private void UpdateHighScore()
+    {
+        if (_score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", _score);
+            _bestScoreText.text = _score.ToString();
+        }
+    }
+
+    public void ScoreUpdate()
+    {
+        _score++;
+        _currentScoreText.text = _score.ToString();
+        UpdateHighScore();
+    }
+}
